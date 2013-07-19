11a349e4a3d967db4afa46be8db8fb7430da6b8b
Geoff Meakin <geoff.meakin@bskyb.com>
2013-07-19 14:13:48 +0100

    Removed bogus hiera-eyaml directory

--- hiera-eyaml/lib/hiera-eyaml.rb
+++
@@ -1,7 +0,0 @@
-require "hiera-eyaml/version"
-
-module Hiera
-  module Eyaml
-    # Your code goes here...
-  end
-end
--- hiera-eyaml/lib/hiera-eyaml/version.rb
+++
@@ -1,5 +0,0 @@
-module Hiera
-  module Eyaml
-    VERSION = "0.0.1"
-  end
-end
