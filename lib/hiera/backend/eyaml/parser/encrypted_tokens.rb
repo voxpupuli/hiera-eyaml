01048324ff300ed2d3dac1dcc80d84ed6338f820
Robert Fielding <fieldingrp@gmail.com>
2013-11-14 18:11:38 +1300

    [FIX] spurious whitespace when saving unchanged blocks

--- lib/hiera/backend/eyaml/parser/encrypted_tokens.rb
+++ lib/hiera/backend/eyaml/parser/encrypted_tokens.rb
@@ -38,6 +38,9 @@ class Hiera
             format = args[:format].nil? ? @format : args[:format]
             case format
               when :block
+                # strip any white space
+                @cipher = @cipher.gsub(/ /m, "")
+                # normalize indentation
                 ciphertext = @cipher.gsub(/\n/, "\n" + @indentation)
                 chevron = (args[:use_chevron].nil? || args[:use_chevron]) ? ">\n" : ''
                 "#{label_string}#{chevron}" + @indentation + "ENC[#{@encryptor.tag},#{ciphertext}]"
