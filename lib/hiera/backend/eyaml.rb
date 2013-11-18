a9e659a77a8cac806df7fa988de269863e51f63e
TomPoulton <tom.poulton@rbi.co.uk>
2013-11-18 14:34:28 -0600

    Updated version number to 1.3.8

--- lib/hiera/backend/eyaml.rb
+++ lib/hiera/backend/eyaml.rb
@@ -2,7 +2,7 @@ class Hiera
   module Backend
     module Eyaml
 
-      VERSION = "1.3.7"
+      VERSION = "1.3.8"
 
       def self.default_encryption_scheme= new_encryption
         @@default_encryption_scheme = new_encryption
