30a9eab4e1cf5568d05053a821d8d6b76d5703d0
Simon Hildrew <simon.hildrew@guardian.co.uk>
2013-11-21 18:39:05 +0000

    Fix issue #54 and improve recovery from simple errors

--- lib/hiera/backend/eyaml/actions/edit_action.rb
+++ lib/hiera/backend/eyaml/actions/edit_action.rb
@@ -3,6 +3,7 @@ require 'hiera/backend/eyaml/actions/decrypt_action'
 require 'hiera/backend/eyaml/actions/encrypt_action'
 require 'hiera/backend/eyaml/options'
 require 'hiera/backend/eyaml/parser/parser'
+require 'highline/import'
 
 class Hiera
   module Backend
@@ -19,50 +20,61 @@ class Hiera
             decrypted_file = Utils.write_tempfile decrypted_input
 
             editor = Utils.find_editor
-            system editor, decrypted_file
-            status = $?
 
-            raise StandardError, "File was moved by editor" unless File.file? decrypted_file
-            edited_file = File.read decrypted_file
-            Utils.secure_file_delete :file => decrypted_file, :num_bytes => [edited_file.length, decrypted_input.length].max
+            begin
+              system "#{editor} #{decrypted_file}"
+              status = $?
 
-            raise StandardError, "Editor #{editor} has not exited?" unless status.exited?
-            raise StandardError, "Editor did not exit successfully (exit code #{status.exitstatus}), aborting" unless status.exitstatus
+              raise StandardError, "File was moved by editor" unless File.file? decrypted_file
+              edited_file = File.read decrypted_file
 
-            raise StandardError, "Edited file is blank" if edited_file.empty?
+              raise StandardError, "Editor #{editor} has not exited?" unless status.exited?
+              raise StandardError, "Editor did not exit successfully (exit code #{status.exitstatus}), aborting" unless status.exitstatus == 0
 
-            if edited_file == decrypted_input
-              Utils.info "No changes detected, exiting"
-            else
-              decrypted_parser = Parser::ParserFactory.decrypted_parser
-              edited_tokens = decrypted_parser.parse(edited_file)
+              raise StandardError, "Edited file is blank" if edited_file.empty?
 
-              # check that the tokens haven't been copy / pasted
-              used_ids = edited_tokens.find_all{ |t| t.class.name =~ /::EncToken$/ }.map{ |t| t.id }
-              if used_ids.length != used_ids.uniq.length
-                  raise StandardError, "A duplicate DEC(ID) was found so I don't know how to proceed. This is probably because you copy and pasted a value - if you do this please delete the ID in parentheses"
-              end
+              if edited_file == decrypted_input
+                Utils.info "No changes detected, exiting"
+              else
+                decrypted_parser = Parser::ParserFactory.decrypted_parser
+                edited_tokens = decrypted_parser.parse(edited_file)
 
-              # replace untouched values with the source values
-              edited_denoised_tokens = edited_tokens.map{ |token|
-                if token.class.name =~ /::EncToken$/ && !token.id.nil?
-                  old_token = tokens[token.id]
-                  if old_token.plain_text.eql? token.plain_text
-                    old_token
+                # check that the tokens haven't been copy / pasted
+                used_ids = edited_tokens.find_all{ |t| t.class.name =~ /::EncToken$/ and !t.id.nil? }.map{ |t| t.id }
+                if used_ids.length != used_ids.uniq.length
+                    raise RecoverableError, "A duplicate DEC(ID) was found so I don't know how to proceed. This is probably because you copy and pasted a value - if you do this please delete the ID in parentheses"
+                end
+
+                # replace untouched values with the source values
+                edited_denoised_tokens = edited_tokens.map{ |token|
+                  if token.class.name =~ /::EncToken$/ && !token.id.nil?
+                    old_token = tokens[token.id]
+                    if old_token.plain_text.eql? token.plain_text
+                      old_token
+                    else
+                      token
+                    end
                   else
                     token
                   end
-                else
-                  token
-                end
-              }
+                }
 
-              encrypted_output = edited_denoised_tokens.map{ |t| t.to_encrypted }.join
+                encrypted_output = edited_denoised_tokens.map{ |t| t.to_encrypted }.join
 
-              filename = Eyaml::Options[:eyaml]
-              File.open("#{filename}", 'w') { |file|
-                file.write encrypted_output
-              }
+                filename = Eyaml::Options[:eyaml]
+                File.open("#{filename}", 'w') { |file|
+                  file.write encrypted_output
+                }
+              end
+            rescue RecoverableError => e
+              Utils.info e
+              if agree "Return to the editor to try again?"
+                retry
+              else
+                raise e
+              end
+            ensure
+              Utils.secure_file_delete :file => decrypted_file, :num_bytes => [edited_file.length, decrypted_input.length].max
             end
 
             nil
