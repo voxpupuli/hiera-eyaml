e292a0c7247284d9e4a4d7bf368c1614a13a55bd
Geoff Meakin <geoff.meakin@bskyb.com>
2013-11-27 17:33:41 +0000

    Moved recrypt action to recrypt command

--- lib/hiera/backend/eyaml/subcommands/recrypt.rb
+++ lib/hiera/backend/eyaml/subcommands/recrypt.rb
@@ -1,17 +1,37 @@
-require 'hiera/backend/eyaml/utils'
-require 'hiera/backend/eyaml/actions/decrypt_action'
-require 'hiera/backend/eyaml/actions/encrypt_action'
+require 'hiera/backend/eyaml/subcommand'
 require 'hiera/backend/eyaml/options'
 require 'hiera/backend/eyaml/parser/parser'
 
 class Hiera
   module Backend
     module Eyaml
-      module Actions
+      module Subcommands
 
-        class RecryptAction
+        class Recrypt < Subcommand
 
-          def self.execute
+          def self.options
+            []
+          end
+
+          def self.description
+            "recrypt an eyaml file"
+          end
+
+          def self.validate options
+            if ARGV.empty?
+              Trollop::die "You must specify an eyaml file" unless options[:eyaml]
+              options[:source] = :eyaml
+              options[:input_data] = File.read options[:eyaml]
+            else
+              Trollop::die "You cannot specify --eyaml, and an eyaml file as an argument" if options[:eyaml]
+              options[:source] = :eyaml
+              options[:eyaml] = ARGV.shift
+              options[:input_data] = File.read options[:eyaml]
+            end
+            options
+          end
+
+          def self.execute 
 
             encrypted_parser = Parser::ParserFactory.encrypted_parser
             tokens = encrypted_parser.parse Eyaml::Options[:input_data]
