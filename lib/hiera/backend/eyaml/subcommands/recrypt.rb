c2592064f79687c9c7e67770246c7ecdf7169f8c
Simon Hildrew <simon.hildrew@guardian.co.uk>
2013-11-28 11:59:59 +0000

    Add test for recrypt and no longer write to disk

--- lib/hiera/backend/eyaml/subcommands/recrypt.rb
+++ lib/hiera/backend/eyaml/subcommands/recrypt.rb
@@ -36,15 +36,9 @@ class Hiera
             encrypted_parser = Parser::ParserFactory.encrypted_parser
             tokens = encrypted_parser.parse Eyaml::Options[:input_data]
             decrypted_input = tokens.each_with_index.to_a.map{|(t,index)| t.to_decrypted :index => index}.join
-            decrypted_file = Utils.write_tempfile decrypted_input
-
-            edited_file = File.read decrypted_file
-            Utils.secure_file_delete :file => decrypted_file, :num_bytes => [edited_file.length, decrypted_input.length].max
-
-            raise StandardError, "Edited file is blank" if edited_file.empty?
 
             decrypted_parser = Parser::ParserFactory.decrypted_parser
-            edited_tokens = decrypted_parser.parse(edited_file)
+            edited_tokens = decrypted_parser.parse(decrypted_input)
 
             encrypted_output = edited_tokens.map{ |t| t.to_encrypted }.join
 
