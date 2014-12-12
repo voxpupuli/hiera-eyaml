9808c9304ff459d987ae81e39f348b3cb925c1ab
TomPoulton <poulton.tm@gmail.com>
2014-12-12 10:40:36 -0600

    Refactored plugins class to use gem methods in utils

--- lib/hiera/backend/eyaml/plugins.rb
+++ lib/hiera/backend/eyaml/plugins.rb
@@ -22,54 +22,36 @@ class Hiera
           @@commands
         end
 
-        def self.register_options args
-          options = args[ :options ]
-          plugin = args[ :plugin ]
+        def self.register_options(args)
+          options = args[:options]
+          plugin = args[:plugin]
           options.each do |name, option_hash|
             option_name = "#{plugin}_#{name}"
             new_option = {:name => option_name}
             new_option.merge! option_hash
-            Hiera::Backend::Eyaml::Utils.warn "Duplicate option #{name} for #{plugin} plugin" if self.option_exists? option_name
+            Hiera::Backend::Eyaml::Utils.warn "Duplicate option #{name} for #{plugin} plugin" if option_exists? option_name
             @@options << new_option
           end
         end
 
-        def self.option_exists? option_name
+        def self.find
+          gem_specs = Hiera::Backend::Eyaml::Utils.find_gem_specs
+          gem_specs.each { |gem_spec|
+            next if @@plugins.include? gem_spec
+
+            file = Hiera::Backend::Eyaml::Utils.find_file_in_gem gem_spec, '**/eyaml_init.rb'
+            next unless file
+
+            @@plugins << gem_spec
+            load file
+          }
+          @@plugins
+        end
+
+        def self.option_exists?(option_name)
           @@options.select { |option| option[:name] == option_name }.count > 0
         end
-
-        def self.find
-
-          this_version = Gem::Version.create(Hiera::Backend::Eyaml::VERSION)
-          index = Gem::VERSION >= "1.8.0" ? Gem::Specification : Gem.source_index
-
-          [index].flatten.each do |source|
-            specs = Gem::VERSION >= "1.6.0" ? source.latest_specs(true) : source.latest_specs
-
-            specs.each do |spec|
-              next if @@plugins.include? spec
-
-              dependency = spec.dependencies.find { |d| d.name == "hiera-eyaml" }
-              next if dependency && !dependency.requirement.satisfied_by?( this_version )
-
-              file = nil
-              if Gem::VERSION >= "1.8.0"
-                file = spec.matches_for_glob("**/eyaml_init.rb").first
-              else
-                file = Gem.searcher.matching_files(spec, "**/eyaml_init.rb").first
-              end
-
-              next unless file
-
-              @@plugins << spec
-              load file
-            end
-
-          end
-
-          @@plugins
-
-        end
+        private_class_method :option_exists?
 
       end
     end
--- spec/hiera/backend/eyaml/plugins_spec.rb
+++ spec/hiera/backend/eyaml/plugins_spec.rb
@@ -2,7 +2,6 @@ require 'spec/spec_helper'
 require 'hiera/backend/eyaml/plugins'
 require 'hiera/backend/eyaml/utils'
 
-
 class Hiera
   module Backend
     module Eyaml
@@ -11,27 +10,38 @@ class Hiera
         def self.clear_options
           @@options = []
         end
+        def self.clear_plugins
+          @@plugins = []
+        end
       end
 
       describe Plugins do
 
-        let(:plugin_option_1) { {
-            :option_1 => { :desc => 'Test option 1',
-                           :type => :string,
-                           :default => 'testing123' }
-        } }
+        before(:each) do
+          Plugins.clear_options
+          Plugins.clear_plugins
+        end
+
+        after(:each) do
+          Plugins.clear_options
+          Plugins.clear_plugins
+        end
 
         describe '.register_options' do
 
-          before(:each) do
-            Plugins.clear_options
+          let(:plugin_option_1) {
+            { :option_1 => { :desc => 'Test option 1', :type => :string, :default => 'testing123' } }
+          }
+
+          let(:plugin_option_2) {
+            { :option_2 => { :desc => 'Test option 2', :type => :string, :default => 'testing456' } }
+          }
+
+          def option_occurrences(options, name)
+            options.select { |option| option[:name] == name }.count
           end
 
-          after(:each) do
-            Plugins.clear_options
-          end
-
-          it 'adds option to options hash' do
+          it 'adds option to options array' do
             Plugins.register_options :options => plugin_option_1, :plugin => ''
 
             expect(Plugins.options.count).to be 1
@@ -53,6 +63,15 @@ class Hiera
             expect(option[:default]).to eq 'testing123'
           end
 
+          it 'adds multiple options to options array' do
+            options = plugin_option_1.merge plugin_option_2
+            Plugins.register_options :options => options, :plugin => 'tester'
+
+            expect(Plugins.options.count).to eq 2
+            expect(option_occurrences(Plugins.options, 'tester_option_1')).to eq 1
+            expect(option_occurrences(Plugins.options, 'tester_option_2')).to eq 1
+          end
+
           it 'warns about duplicate options' do
             Hiera::Backend::Eyaml::Utils.expects(:warn).with('Duplicate option option_1 for tester plugin').once
 
@@ -62,8 +81,55 @@ class Hiera
 
         end
 
-      end
+        describe '.find' do
 
+          let(:gem_specs) { [] }
+          let(:gem_spec_1) { mock('Gem::Specification') }
+          let(:file_path) { '/path/to/plugin_file' }
+
+          before(:each) do
+            gem_spec_1.stubs(:name).returns('spec_1')
+            gem_specs << gem_spec_1
+            Hiera::Backend::Eyaml::Utils.stubs(:find_gem_specs).returns(gem_specs)
+            Hiera::Backend::Eyaml::Utils.stubs(:find_file_in_gem).returns(nil)
+            Hiera::Backend::Eyaml::Plugins.stubs(:load)
+          end
+
+          context 'when a gem does not contain plugin file' do
+            it 'excludes gem_spec from plugins list' do
+              Plugins.find
+              expect(Plugins.plugins.count).to eq 0
+            end
+          end
+
+          context 'when a gem contains plugin file' do
+            before(:each) do
+              Hiera::Backend::Eyaml::Utils.stubs(:find_file_in_gem).returns(file_path)
+            end
+
+            it 'adds gem_spec to plugins list' do
+              Plugins.find
+              expect(Plugins.plugins.count).to eq 1
+              expect(Plugins.plugins.first.name).to eq gem_spec_1.name
+            end
+
+            it 'loads the plugin file' do
+              Hiera::Backend::Eyaml::Plugins.expects(:load).with(file_path).once
+              Plugins.find
+            end
+          end
+
+          context 'when a plugin gem is listed more than once' do
+            it 'only includes the gem once' do
+              gem_specs << gem_spec_1
+              Hiera::Backend::Eyaml::Utils.stubs(:find_file_in_gem).returns(file_path)
+              Plugins.find
+              expect(Plugins.plugins.count).to eq 1
+            end
+          end
+        end
+
+      end
     end
   end
 end
