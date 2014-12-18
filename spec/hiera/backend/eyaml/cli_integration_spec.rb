4ea7d56bd20b781b7f8873ebbea3e59ef360bae8
TomPoulton <poulton.tm@gmail.com>
2014-12-18 15:29:59 -0600

    Updated cli integration tests

--- spec/hiera/backend/eyaml/cli_integration_spec.rb
+++ spec/hiera/backend/eyaml/cli_integration_spec.rb
@@ -6,26 +6,50 @@ require 'hiera/backend/eyaml/encryptors/pkcs7'
 class Hiera
   module Backend
     module Eyaml
+
+      class Plugins
+        def self.clear_all
+          @@plugins = []
+          @@commands = []
+          @@options = []
+        end
+      end
+      module Subcommands
+        def self.clear_all
+          @@subcommands = {}
+        end
+      end
+
       describe CLI do
 
-        describe '.execute' do
-          before(:each) do
-            File.stubs(:read).with('./keys/public_key.pkcs7.pem').returns Eyaml::Keys.pkcs7_public_key
-            File.stubs(:read).with('./keys/private_key.pkcs7.pem').returns Eyaml::Keys.pkcs7_private_key
-          end
-          # Sample Integration test
-          it 'encrypts a string' do
-            ARGV.clear
-            ARGV.push('encrypt')
-            ARGV.push('-s')
-            ARGV.push('hello')
+        before(:each) do
+          File.stubs(:read).with('./keys/public_key.pkcs7.pem').returns Eyaml::Keys.pkcs7_public_key
+          File.stubs(:read).with('./keys/private_key.pkcs7.pem').returns Eyaml::Keys.pkcs7_private_key
 
-            Hiera::Backend::Eyaml::Encryptors::Pkcs7.register
-            expect {
-              CLI.parse
-              CLI.execute
-            }.to output(/string: ENC\[PKCS7,.*\]/).to_stdout
-          end
+          Plugins.clear_all
+          Subcommands.clear_all
+          Hiera::Backend::Eyaml::Encryptors::Pkcs7.register
+          ARGV.clear
+        end
+
+        it 'encrypts a string' do
+          ARGV.push('encrypt')
+          ARGV.push('-s')
+          ARGV.push('hello')
+
+          expect {
+            CLI.parse
+            CLI.execute
+          }.to output(/string: ENC\[PKCS7,.*\]/).to_stdout
+        end
+
+        it 'returns help info' do
+          ARGV.push('help')
+
+          expect {
+            CLI.parse
+            CLI.execute
+          }.to output(/Welcome to eyaml.*\n+Usage:/).to_stdout
         end
 
       end
