a45bb4cffa360cb2e6d1fc5fc63baa714d8918ad
TomPoulton <poulton.tm@gmail.com>
2015-03-03 22:20:48 -0600

    Set version to 2.0.7

--- lib/hiera/backend/eyaml.rb
+++ lib/hiera/backend/eyaml.rb
@@ -2,7 +2,7 @@ class Hiera
   module Backend
     module Eyaml
 
-      VERSION = "2.0.6"
+      VERSION = "2.0.7"
       DESCRIPTION = "Hiera-eyaml is a backend for Hiera which provides OpenSSL encryption/decryption for Hiera properties"
 
       class RecoverableError < StandardError
