667bca807a2ffdbacf7d133f9a3f63a1a44a80d5
Andrew Reenan <andrew.reenan@gettyimages.com>
2016-10-19 18:07:40 -0700

    Bumping version

--- lib/hiera/backend/eyaml.rb
+++ lib/hiera/backend/eyaml.rb
@@ -2,7 +2,7 @@ class Hiera
   module Backend
     module Eyaml
 
-      VERSION = "2.1.1"
+      VERSION = "2.1.2"
       DESCRIPTION = "Hiera-eyaml is a backend for Hiera which provides OpenSSL encryption/decryption for Hiera properties"
 
       class RecoverableError < StandardError
