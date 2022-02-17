039e39731862219a860044dde00bff3d22a21938
David Sansome <me@davidsansome.com>
2022-02-17 16:11:47 +1100

    Do Gem::Version comparisons instead of string comparisons

--- lib/hiera/backend/eyaml/plugins.rb
+++ lib/hiera/backend/eyaml/plugins.rb
@@ -25,21 +25,22 @@ class Hiera
 
         def self.find
 
+          gem_version = Gem::Version.new(Gem::VERSION)
           this_version = Gem::Version.create(Hiera::Backend::Eyaml::VERSION)
-          index = Gem::VERSION >= "1.8.0" ? Gem::Specification : Gem.source_index
+          index = gem_version >= Gem::Version.new("1.8.0") ? Gem::Specification : Gem.source_index
 
           [index].flatten.each do |source|
-            specs = Gem::VERSION >= "1.6.0" ? source.latest_specs(true) : source.latest_specs
+            specs = gem_version >= Gem::Version.new("1.6.0") ? source.latest_specs(true) : source.latest_specs
 
             specs.each do |stub|
-              spec = Gem::VERSION >= "3.2.33" ? stub.to_spec : stub
+              spec = gem_version >= Gem::Version.new("3.2.33") ? stub.to_spec : stub
               next if @@plugins.include? spec
 
               dependency = spec.dependencies.find { |d| d.name == "hiera-eyaml" }
               next if dependency && !dependency.requirement.satisfied_by?( this_version )
 
               file = nil
-              if Gem::VERSION >= "1.8.0"
+              if gem_version >= Gem::Version.new("1.8.0")
                 file = spec.matches_for_glob("**/eyaml_init.rb").first
               else
                 file = Gem.searcher.matching_files(spec, "**/eyaml_init.rb").first
